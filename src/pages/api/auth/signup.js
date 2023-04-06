a5803ab2f9cf3525efed96648d54a426c645fbcc
hamidazimiazp <hamidazimi.azp@gmail.com>
2023-04-06 13:48:55 +0430

    Continuation of the signup api

--- src/pages/api/auth/signup.js
+++ src/pages/api/auth/signup.js
@@ -1,3 +1,4 @@
+import User from "@/models/User";
 import connectDB from "@/utils/connectDB";
 
 export default async function handler(req, res) {
@@ -10,8 +11,23 @@ export default async function handler(req, res) {
     console.log(error.message);
     return res
       .status(500)
-      .json({ status: "Failde!", message: "Server Error !" });
+      .json({ status: "Failed!", message: "Server Error !" });
   }
 
   const { email, password, confirmPassword } = req.body;
+
+  if (!email || !password || confirmPassword || password !== confirmPassword) {
+    return res
+      .status(400)
+      .json({ status: "Failed!", message: "Invalid Data !" });
+  }
+
+  const user = await User.create({
+    email: email,
+    password: password,
+    fullName: "",
+    bio: "",
+  });
+
+  res.status(201).json({ status: "Success!", message: "Created", data: user });
 }

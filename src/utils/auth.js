35d586cbb4304cf7df5486d61f5431beb4f805b7
hamidazimiazp <hamidazimi.azp@gmail.com>
2023-04-06 16:09:20 +0430

    add verifyPassword fun to utils

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,8 +1,13 @@
-import { hash } from "bcryptjs";
+import { compare, hash } from "bcryptjs";
 
 const hashPasssword = async (password) => {
   const newPass = await hash(password, 12);
   return newPass;
 };
 
-export { hashPasssword };
+const verifyPassword = async (password, hashPass) => {
+  const newPass = await compare(password, hashPass);
+  return newPass;
+};
+
+export { hashPasssword, verifyPassword };

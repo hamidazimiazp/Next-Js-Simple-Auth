a57b58335862ad70edb2d048cb988293dc8a33ed
hamidazimiazp <hamidazimi.azp@gmail.com>
2023-04-06 16:33:13 +0430

    add user verify api

---
+++ src/pages/api/user.js
@@ -0,0 +1,31 @@
+import User from "@/models/User";
+import { verifyToken } from "@/utils/auth";
+
+export default async function handler(req, res) {
+  if (req.method !== "GET") return;
+
+  const { token } = req.cookie;
+  const secretKey = process.env.SECRET_KEY;
+
+  if (!token) {
+    return res.status(401).json({
+      status: "failed",
+      message: "your not logged in",
+    });
+  }
+
+  const result = verifyToken(token, secretKey);
+
+  if (result) {
+    const user = await User.findOne({ email: result.email });
+    res.status(200).json({
+      status: "success",
+      data: user,
+    });
+  } else {
+    res.status(401).json({
+      status: "failed",
+      message: "your unauthorized !",
+    });
+  }
+}

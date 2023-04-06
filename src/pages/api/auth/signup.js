14badef0bb0d36e00379c5ce789af7a1d64fd739
hamidazimiazp <hamidazimi.azp@gmail.com>
2023-04-06 13:36:50 +0430

    use connect db func in signup api

--- src/pages/api/auth/signup.js
+++ src/pages/api/auth/signup.js
@@ -1,3 +1,17 @@
+import connectDB from "@/utils/connectDB";
+
 export default async function handler(req, res) {
   if (req.method !== "POST") return;
+
+  // connect database
+  try {
+    await connectDB();
+  } catch (error) {
+    console.log(error.message);
+    return res
+      .status(500)
+      .json({ status: "Failde!", message: "Server Error !" });
+  }
+
+  const { email, password, confirmPassword } = req.body;
 }

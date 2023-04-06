6fa87657eb458b57102c06293f54ac0368c3dab4
hamidazimiazp <hamidazimi.azp@gmail.com>
2023-04-06 16:25:53 +0430

    add dashboard page

---
+++ src/components/templates/HomePage/DashboardPage.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+const styles = {
+  width: "100%",
+  height: "100%",
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  flexDirection: "column",
+  gap: 10,
+  color: "#1e1e1e",
+  fontSize: 26,
+  fontWeight: "normal",
+};
+
+const Dashboard = () => {
+  return (
+    <div style={styles}>
+      <h1>Welcome -_-</h1>
+    </div>
+  );
+};
+
+export default Dashboard;
---
+++ src/pages/dashboard.js
@@ -0,0 +1,8 @@
+import Dashboard from "@/components/templates/HomePage/DashboardPage";
+import React from "react";
+
+const dashboard = () => {
+  return <Dashboard />;
+};
+
+export default dashboard;

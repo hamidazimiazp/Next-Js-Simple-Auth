01926ac30cc3c83e84b350057e172154089f230c
hamidazimiazp <hamidazimi.azp@gmail.com>
2023-04-06 15:19:03 +0430

    add signup page

---
+++ src/pages/signup.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "@/styles/auth.module.css";
+
+const SignUp = () => {
+  return (
+    <div className={styles.wrapper}>
+      <form className={styles.form}>200</form>
+    </div>
+  );
+};
+
+export default SignUp;
